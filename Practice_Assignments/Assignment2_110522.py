0b2acf3f103e59bf56e27bc9a456a9762a245582
Sunilkumar C <105116767+sunilcm862@users.noreply.github.com>
2022-05-16 21:03:09 +0530

    Add files via upload

--- Practice_Assignments/Assignment2_110522.py
+++ Practice_Assignments/Assignment2_110522.py
@@ -1,5 +1,5 @@
 # print("")
-# print("not Finished This assignment 100%")
+# print("Finished This assignment 100%")
 # print("")
 # #          Assignment 2 - 11th May 2022
 
@@ -10,6 +10,43 @@
 # # 5. Take the range from the user and print all the even numbers within that range
 # #    Start  10
 # #    End     20
+
+# -----------------------------------1-------------------------------------------------------
+
+# # 1. Take input from the user and find the factorial (using while loop)
+
+# a=int(input("Enter number to calculate factorial :"))
+# print("Factorial of",a,"is : ")
+# fct=1
+# while a>0:
+#      fct=fct*a
+#      a -= 1
+# print(fct)
+
+# -----------------------------------2----------------------------------------------------
+
+# # 2. Take input from the user and print the table of that value (3 * 1 = 3)
+
+# a= int(input("Enter number of table :"))
+# #print(a)
+
+# for i in range(1,11):
+#      b=a*i
+#      #print(i,"  ",b)
+#      print(a," * ",i," = ",b)
+
+# ------------------------------------3------------------------------------------------------
+
+# # 3. Take input from the user and reverse the value (input = 789 , output = 987)
+# a_3=int(input("enter value to reverse : "))
+# rev=0
+# while a_3>0:
+#      b_3 = a_3%10
+#      rev = (rev*10)+b_3
+#      print(b_3,rev)
+#      a_3= a_3 // 10
+# print(rev)
+
 # #-----------------------------------4---------------------------------------------------
 # # 4. Take input from the user and check whether it is an even number or an odd number
 # a = input("Enter a number : ")
@@ -24,16 +61,14 @@
 # #   Start  10
 # #   End     20
 
-a=int(input("Start : "))
-b= int(input("End : "))
-c=0
-lst=list(range(a,b+1))
-print(lst)
-for i in lst:
-     if(i%2 == 0):
-          print(i,"is Even number")
-          c +=1
+# a=int(input("Start : "))
+# b= int(input("End : "))
+# c=0
+# lst=list(range(a,b+1))
+# print(lst)
+# for i in lst:
+#      if(i%2 == 0):
+#           print(i,"is Even number")
+#           c +=1
 
-print(c)
-# ------------------------------------------------------------------------------------------
-     
+# print(c)
--- Practice_Assignments/Assignment3_120522.py
+++ Practice_Assignments/Assignment3_120522.py
@@ -18,10 +18,27 @@
 # #         *
 # #         *
 
-# #--------------------------------------------2----------------------------------------------
+# #--------------------------------------------1----------------------------------------------
+# 1. Take input from a user and check whether it is a palindrome or not
+# #  (121 121  = palindrome)
+
+# a=int(input("Enter number : "))
+# c=a
+# rev=0
+# while a>0:
+#      b=a%10
+#      rev=rev*10+b
+#      a=a//10
+# if(c==rev):
+#      print(c," is palindrome")
+# else:
+#      print(c,"is not palindrome")
+
+# #--------------------------------------------2  VVImp----------------------------------------------
 # # 2. Take input from the user and check whether it is an even number or an odd number 
 # # (% operator not allowed here)
-# # true or false method
+
+# #>>>>>> true or false method
 # a = int(input("Enter a number : "))
 # print(a&1)
 # if((a&1) ==0):
@@ -31,3 +48,6 @@
 
 # #------------------------------------------------------------------------------------------
 # 3. Take the range from the user and print all the prime numbers within that range
+
+# x = int(input("Enter a number : "))
+# y = int(input("Enter a number : "))
